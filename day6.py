dcfc28b0e437157c46a18998167138b1cf522264
Nicolas Smith <nicolasasmith@gmail.com>
2020-12-13 08:31:13 -0500

    Rewrite

--- day6.py
+++ day6.py
@@ -1,36 +1,25 @@
+# Solution for https://adventofcode.com/2020/day/6
+
 import os
 
 
 def solve(file):
-    group_answer_info = []
+    sum_unique_answers = 0
+    sum_common_answers = 0
     with open(file) as f:
-        group_answers = f.read().split(os.linesep + os.linesep)
-        for group_answer in group_answers:
-            unique_answers = set()
-            all_answers = {}
-            individual_answers = group_answer.rstrip().split(os.linesep)
-            for answers in individual_answers:
-                for answer in list(answers):
-                    unique_answers.add(answer)
-                    if answer in all_answers:
-                        all_answers[answer] += 1
-                    else:
-                        all_answers[answer] = 1
-            count_every = sum(all_answers[c] == len(individual_answers) for c in all_answers)
-            group_answer_info.append((len(unique_answers), count_every))
-
-    any_yes = 0
-    all_yes = 0
-    for a in group_answer_info:
-        any_yes += a[0]
-        all_yes += a[1]
-    return any_yes, all_yes
+        lines = f.read().split(os.linesep + os.linesep)
+        for line in lines:
+            line = line.rstrip().split(os.linesep)
+            answers = list(map(set, line))
+            sum_unique_answers += len(set.union(*answers))
+            sum_common_answers += len(set.intersection(*answers))
+    return sum_unique_answers, sum_common_answers
 
 
 def main():
-    part1, part2 = solve('data/day6-input.txt')
-    print(f"Part1: {part1}")
-    print(f"Part2: {part2}")
+    unique_answers, common_answers = solve('data/day6-input.txt')
+    print(f"Part1, sum of unique answers: {unique_answers}")
+    print(f"Part2, sum of common answers: {common_answers}")
 
 
 if __name__ == '__main__':
